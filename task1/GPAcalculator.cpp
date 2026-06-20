5bab5457d029fc2363f064bc5532717977514e08
coder-Retro <hasnainqadri9c@gmail.com>
2026-06-20 16:56:46 +0500

    Add: Initial Setup

---
+++ task1/GPAcalculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+// Course Class
+class Course {
+    
+};
+// Helper Functions
+float sum() {
+
+}
+float pro() {
+
+}
+void result() {
+    
+}
+// Main Function
+int main() {
+
+    return 0;
+}
