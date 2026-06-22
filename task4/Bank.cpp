8e20d31e503aa5cf3a50fcad359b6a9c62963d81
coder-Retro <hasnainqadri9c@gmail.com>
2026-06-22 15:48:38 +0500

    Add: Initialized task 4

--- task4/Bank.cpp
+++ task4/Bank.cpp
@@ -6,17 +6,28 @@ using namespace std;
 // Customer Class
 class Customer {
 private:
-
+    string cID;
+    string name;
+    unsigned int age;
+    char gender;
+    vector<Account*> acc;
 public:
-
+    Customer(const string& cID,const string& n,const unsigned int& a,const char& g):{
+        this->cID=cID;
+        name=n;
+        age=a;
+        gender=g;
+    }
 };
 
 // Account Class
 class Account {
 private:
-
+    const string IBN;
+    long double balance;
+    vector<Transaction*> tran;
 public:
-
+    Account(const string& ibn): IBN(ibn), balance(0) {}
 };
 
 // Transaction Class
